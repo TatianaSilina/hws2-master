d8ff801f8578b68e2b7adc17b9dd053077955669
TatianaSilina <persefonochka88@mail.ru>
2023-01-29 17:57:10 +0300

    commit - Home work 4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,9 +17,15 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = `${s.button} ${disabled && s.disabled} 
-        ${xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : '' + s.default}
-        ${className && className}`// задачка на смешивание классов
+    const finalClassName = s.button
+        + (disabled
+                ? ' ' + s.disabled
+                : xType === 'red'
+                    ? ' ' + s.red : xType === 'secondary' ? ' ' + s.secondary : '' + s.default
+
+                        + (className ? ' ' + className : '')
+        )
+    // задачка на смешивание классов
 
     return (
         <button
