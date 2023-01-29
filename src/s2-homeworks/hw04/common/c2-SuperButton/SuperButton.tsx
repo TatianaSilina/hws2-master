38bf92d8fe4f77119a4a3e2354b935f001be93d6
TatianaSilina <persefonochka88@mail.ru>
2023-01-29 17:58:50 +0300

    commit - Home work 4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -21,7 +21,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         + (disabled
                 ? ' ' + s.disabled
                 : xType === 'red'
-                    ? ' ' + s.red : xType === 'secondary' ? ' ' + s.secondary : '' + s.default
+                    ? ' ' + s.red : xType === 'secondary' ? ' ' + s.secondary : ' ' + s.default
 
                         + (className ? ' ' + className : '')
         )
