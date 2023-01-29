38cb8792742655617524d10fbcb81b5d636115af
TatianaSilina <persefonochka88@mail.ru>
2023-01-29 17:16:29 +0300

    commit - Home work 4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,11 +17,9 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = `
-    ${s.button} ${disabled && s.disabled}
-    ${xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : s.default}
-    ${className && className}
-    `
+    const finalClassName = `${s.button} ${disabled && s.disabled} 
+        ${xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : s.default}
+        ${className && className}`
 
 
     return (
