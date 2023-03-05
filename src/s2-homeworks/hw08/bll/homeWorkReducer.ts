d5843ff1c0896457f140c698e87ba8487e25b7ff
TatianaSilina <persefonochka88@mail.ru>
2023-03-05 22:10:14 +0300

    commit - Home work 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -20,7 +20,7 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
         }
         case 'check': {
 
-            return state.filter(el => el.age > action.payload)
+            return state.filter(el => el.age >= action.payload)
         }
         default:
             return state
