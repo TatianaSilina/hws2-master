efe9ed3328dc1d943fa3f6ca8a73c1fe02f7b4a0
TatianaSilina <persefonochka88@mail.ru>
2023-06-22 17:32:36 +0300

    commit - Home work 14

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -16,7 +16,7 @@ import {useSearchParams} from 'react-router-dom'
 const getTechs = (find: string) => {
     return axios
         .get<{ techs: string[] }>(
-            'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test2',
+            'https://samurai.it-incubator.io/api/3.0/homework/test2',
             {params: {find}}
         )
         .catch((e) => {
@@ -27,29 +27,24 @@ const getTechs = (find: string) => {
 const HW14 = () => {
     const [find, setFind] = useState('')
     const [isLoading, setLoading] = useState(false)
-    const [searchParams, setSearchParams] = useSearchParams()
+    const [searchParams, setSearchtechsParams] = useSearchParams()
     const [techs, setTechs] = useState<string[]>([])
 
     const sendQuery = (value: string) => {
         setLoading(true)
         getTechs(value)
             .then((res) => {
-                // делает студент
+                if(res){
+                    setTechs(res.data.techs)
+                }
+                setLoading(false)
 
-                // сохранить пришедшие данные
-
-                //
             })
     }
 
     const onChangeText = (value: string) => {
         setFind(value)
-        // делает студент
-
-        // добавить/заменить значение в квери урла
-        // setSearchParams(
-
-        //
+        setSearchtechsParams(value)
     }
 
     useEffect(() => {
--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -32,7 +32,10 @@ const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
         onChangeText?.(value)
 
         if (onDebouncedChange) {
-            // делает студент
+            clearTimeout(timerId)
+            setTimerId(+(setTimeout(()=>{
+                onDebouncedChange(value)
+            }, 1500)))
 
             // остановить предыдущий таймер
             // запустить новый на 1500ms, в котором вызовется функция
